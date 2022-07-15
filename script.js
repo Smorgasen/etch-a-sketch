c8da5a257b9693d0c690adf2551a701935b58b0b
Smorgasen <yaroslaw.ivanko@gmail.com>
2022-07-15 21:28:41 +0300

    Fix formula for readability (less variables)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('.container');
 
 function createGrid(gridNumber) {
-    let gridArea = (gridNumber * gridNumber);
-    for (let i = 1; i <= gridArea; i++) {
+    // let gridArea = (gridNumber * gridNumber);
+    for (let i = 1; i <= (gridNumber * gridNumber); i++) {
         let gridItem = document.createElement('div');
         container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
@@ -10,4 +10,4 @@ function createGrid(gridNumber) {
     }
 }
 
-createGrid(16)
+createGrid(4)
