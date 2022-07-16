b0f9e7bd77d0e6fee6fda51af276c8e02c5662d9
Smorgasen <yaroslaw.ivanko@gmail.com>
2022-07-16 16:43:35 +0300

    Add slider for choosing the grid size

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const container = document.querySelector('.container');
 
 function createGrid(gridNumber) {
-    // let gridArea = (gridNumber * gridNumber);
-    for (let i = 1; i <= (gridNumber * gridNumber); i++) {
-        let gridItem = document.createElement('div');
+    let gridArea = (gridNumber * gridNumber);
+    for (let i = 1; i <= gridArea; i++) {
+        gridItem = document.createElement('div');
         container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
-        container.appendChild(gridItem)
+        container.appendChild(gridItem);
     }
     container.addEventListener('mouseover', event => {
-        let item = event.target
+        let item = event.target;
         if (item != container) {
-            item.classList.add('permahover')
+            item.classList.add('permahover');
         }
     })
 }
 
-createGrid(16)
+let slider = document.getElementById('range');
+let outputLeft = document.getElementById('grid-size-left');
+let outputRight = document.getElementById('grid-size-right');
+outputLeft.innerHTML = slider.value;
+outputRight.innerHTML = slider.value;
+
+slider.oninput = function() {
+    outputLeft.innerHTML = slider.value;
+    outputRight.innerHTML = slider.value;
+}
+
+function gridSize() {
+    let gridPixels = container.querySelectorAll('div');
+    gridPixels.forEach(gridPixel => gridPixel.remove());
+    createGrid(slider.value);
+}
+
+slider.addEventListener('mouseup', gridSize)
+createGrid (slider.value)
+
+
+
+
+
